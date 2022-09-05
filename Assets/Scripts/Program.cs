d7a525095320fa0a86c665b10d6cf40f2b4070f6
Katopan <76249494+katomizuki@users.noreply.github.com>
2022-09-05 21:55:55 +0900

    add shader

---
+++ Assets/Scripts/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+public class Program : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+class ProgramPractice
+{
+    static void Main(string[] args)
+    {
+        // 
+        for (int feet = 1; feet <= 10; feet++)
+        {
+            Console.WriteLine(feet);
+        }
+    }
+}
